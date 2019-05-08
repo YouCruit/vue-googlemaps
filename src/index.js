d1acd24d293dbc1bcc5dd91062ca4aebd4423f3a
Andreas Holmberg <andreas.holmberg@youcruit.com>
2019-05-08 12:23:37 +0200

    Added load external param

--- src/index.js
+++ src/index.js
@@ -59,21 +59,8 @@ const plugin = {
 			registerComponents(Vue, finalOptions.componentsPrefix)
 		}
 
-		if (finalOptions.load) {
-			loader.load(finalOptions.load)
-		}
+		loader.load(finalOptions.load)
 	},
 }
 
 export default plugin
-
-// Auto-install
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue
-}
-if (GlobalVue) {
-	GlobalVue.use(plugin)
-}
--- src/lib-loader.js
+++ src/lib-loader.js
@@ -1,6 +1,10 @@
+const ASSERT_DELAY = 50
+const ASSERT_TIMEOUT = 30000
+
 const loader = {
 	loaded: false,
 	readyPromises: [],
+	assertRetries: Math.floor(ASSERT_TIMEOUT / ASSERT_DELAY),
 
 	/**
 	 * @param apiKey    API Key, or object with the URL parameters. For example
@@ -14,68 +18,87 @@ const loader = {
 	 * @param loadCn    Boolean. If set to true, the map will be loaded form goole maps China
 	 *                  (@see https://developers.google.com/maps/documentation/javascript/basics#GoogleMapsChina)
 	 */
-	load ({ apiKey, version, libraries, loadCn, useNewFeatures = true }) {
+	load ({ apiKey, version, libraries, loadCn, useNewFeatures = true, externalGoogleMaps = true }) {
 		if (typeof window === 'undefined') {
 			// Do nothing if run from server-side
 			return Promise.resolve()
 		}
 		if (!this.loaded && (!window.google || !window.google.maps)) {
-			const googleMapScript = document.createElement('SCRIPT')
-
-			// Allow apiKey to be an object.
-			// This is to support more esoteric means of loading Google Maps,
-			// such as Google for business
-			// https://developers.google.com/maps/documentation/javascript/get-api-key#premium-auth
-			var options = {}
-			if (typeof apiKey === 'string') {
-				options.key = apiKey
-			} else if (typeof apiKey === 'object') {
-				for (let k in apiKey) { // transfer values in apiKey to options
-					options[k] = apiKey[k]
-				}
+			if (externalGoogleMaps) {
+				this._assertGoogleMapsLoaded()
 			} else {
-				throw new Error('`apiKey` should either be a string or an object')
+				this._loadScript(apiKey, version, libraries, loadCn, useNewFeatures)
 			}
-
-			// libraries
-			let librariesPath = ''
-			if (libraries && libraries.length > 0) {
-				librariesPath = libraries.join(',')
-				options['libraries'] = librariesPath
-			} else if (Array.prototype.isPrototypeOf(options.libraries)) {
-				options.libraries = options.libraries.join(',')
-			}
-			options['callback'] = 'VueGoogleMapsLoaded'
-
-			const baseUrl = (typeof loadCn === 'boolean' && loadCn === true)
-				? 'http://maps.google.cn'
-				: 'https://maps.googleapis.com'
-
-			const urlParams = Object.keys(options)
-				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`)
-				.join('&')
-
-			let url = `${baseUrl}/maps/api/js?${urlParams}`
-
-			// Override version if they do not want to use the new renderer/base map
-			if (!useNewFeatures) {
-				version = '3.31'
-			}
-
-			if (version) {
-				url = `${url}&v=${version}`
-			}
-
-			googleMapScript.setAttribute('src', url)
-			googleMapScript.setAttribute('async', '')
-			googleMapScript.setAttribute('defer', '')
-			document.body.appendChild(googleMapScript)
-
-			window.VueGoogleMapsLoaded = this._setLoaded.bind(this)
 		} else {
-			console.warn('The Google Maps library is already loaded')
+			console.info('The Google Maps library is already loaded')
 			this._setLoaded()
 		}
+	},
+
+	_assertGoogleMapsLoaded() {
+		if (window.google && window.google.maps) {
+			this._setLoaded()
+		} else if (this.retries > 0) {
+			this.retries--
+			window.setTimeout(this._assertGoogleMapsLoaded, ASSERT_DELAY)
+		} else {
+			this._assertFailed()
+		}
+	},
+
+	_loadScript(apiKey, version, libraries, loadCn, useNewFeatures) {
+		const googleMapScript = document.createElement('SCRIPT')
+
+		// Allow apiKey to be an object.
+		// This is to support more esoteric means of loading Google Maps,
+		// such as Google for business
+		// https://developers.google.com/maps/documentation/javascript/get-api-key#premium-auth
+		var options = {}
+		if (typeof apiKey === 'string') {
+			options.key = apiKey
+		} else if (typeof apiKey === 'object') {
+			for (let k in apiKey) { // transfer values in apiKey to options
+				options[k] = apiKey[k]
+			}
+		} else {
+			throw new Error('`apiKey` should either be a string or an object')
+		}
+
+		// libraries
+		let librariesPath = ''
+		if (libraries && libraries.length > 0) {
+			librariesPath = libraries.join(',')
+			options['libraries'] = librariesPath
+		} else if (Array.prototype.isPrototypeOf(options.libraries)) {
+			options.libraries = options.libraries.join(',')
+		}
+		options['callback'] = 'VueGoogleMapsLoaded'
+
+		const baseUrl = (typeof loadCn === 'boolean' && loadCn === true)
+			? 'http://maps.google.cn'
+			: 'https://maps.googleapis.com'
+
+		const urlParams = Object.keys(options)
+			.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(options[key])}`)
+			.join('&')
+
+		let url = `${baseUrl}/maps/api/js?${urlParams}`
+
+		// Override version if they do not want to use the new renderer/base map
+		if (!useNewFeatures) {
+			version = '3.31'
+		}
+
+		if (version) {
+			url = `${url}&v=${version}`
+		}
+
+		googleMapScript.setAttribute('src', url)
+		googleMapScript.setAttribute('async', '')
+		googleMapScript.setAttribute('defer', '')
+		document.body.appendChild(googleMapScript)
+
+		window.VueGoogleMapsLoaded = this._setLoaded.bind(this)
 	},
 
 	ensureReady () {
@@ -96,6 +119,14 @@ const loader = {
 		}
 		this.readyPromises = []
 	},
+
+	_assertFailed() {
+		this.loaded = true
+		for (const reject of this.readyPromises) {
+			reject()
+		}
+		this.readyPromises = []
+	}
 }
 
 export default loader
